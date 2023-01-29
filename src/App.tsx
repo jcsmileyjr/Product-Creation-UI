3fb4caee7eaf4d1f65cc4a11df0ae13ad3fd6c6d
Smiley <jcsmileyjr@gmail.com>
2023-01-29 08:37:45 -0600

    Setup the overall layout of the product visual and UI sections

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,29 @@ import React from 'react';
 import './App.css';
 
 /**
- * First TYPESCRIPT project - Product Creation
+ * First TYPESCRIPT project - Product Creation UI
  * 
- * TODO 1: A user can see the words "Hello World"
+ * TODO 1: DONE- A user can see the words "Hello World"
  * TODO 2: A user can write a new product title through an input field
  * TODO 3: A user can write a brief product description
- * TODO 4: The user can enter a dollar amount
- * TODO 5: A user can check Yes/No if the product is in stock
- * TODO 6: A user can verify their product details (number of units, color, made in USA based), shoud be an object
+ * TODO 4: A user can enter a dollar amount. Only whole dollar amounts.
+ * TODO 5: A user can check Yes/No if the product is in stock. If no in stock, this should be reflected.
+ * TODO 6: A user can verify their product details (number of units, color, made in USA based), shoud be an object.
  * 
  */
 function App() {
   return (
-    <div className="App">
-      <p>Hello World</p>
-    </div>
+    <main>
+      {/*Non input section to display the product based on input from Product UI section */}
+      <section id="product-visual" className=''>
+          Product Visual
+      </section>
+      
+      {/*Product input section that is displayed in the Product Visual Section */}
+      <section id="product-ui">
+          Product UI
+      </section>
+    </main>
   );
 }
 
